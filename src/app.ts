1652e093030c060355a3c4b0ace45ce014ce63cd
Reginaldo Santos <reginaldosantos.br@gmail.com>
2023-04-03 16:49:54 -0300

    add twilio response

--- src/app.ts
+++ src/app.ts
@@ -15,10 +15,23 @@ dotenv.config()
 app.post('/chat/send', async (req, res) => {
     const {to, body} = req.body
     try {
-        const result = await sendWhatsappMessage(`whatsapp:${to}`, body) 
-        console.log(result)
+        await sendWhatsappMessage(`whatsapp:${to}`, body) 
         res.status(200).json({success: true, body})
     }catch (error) {
+        res.status(500).json({success: false, error})
+    }
+})
+
+app.post('/chat/receive', async (req, res) => {
+    const twilioRequestBody = req.body
+    console.log("twilioRequestBody", twilioRequestBody)
+    const messageBody = twilioRequestBody.Body
+    const to = twilioRequestBody.From
+
+    try {
+        await sendWhatsappMessage(to, messageBody)
+        res.status(200).json({success: true, messageBody})
+    } catch (error) {
         res.status(500).json({success: false, error})
     }
 })
